45b3b5bf521420dd84a5659daf9147e12973c86b
Senior-paul <samwel.massawe1111@gmail.com>
2024-03-14 17:30:14 +0300

    first commit

---
+++ website2/home.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+     <!-- custome css file link  -->
+
+     <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body>
+
+<!-- header section stsrts -->
+<section class="header">
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">HOME</a>
+        <a href="about.php">ABOUT</a>
+        <a href="pakage.php">PAKAGE</a>
+        <a href="book.php">BOOK</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+<!-- header section ends  -->
+
+
+
+
+
+
+
+
+    <!-- footer section start here  -->
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+                <a href="pakage.php"><i class="fas fa-angle-right"></i> pakage</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> ask question</a>
+                <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="3"><i class="fas fa-angle-right"></i> term of use</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> ask question</a>
+                <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="3"><i class="fas fa-angle-right"></i> term of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +255-689-670-231</a>
+                <a href="#"><i class="fas fa-phone"></i> +255-718-505-760</a>
+                <a href="#"><i class="fas fa-envelope"></i> sam@gmail.com</a>
+                <a href="3"><i class="fas fa-map"></i> dar-Tz</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i> facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i> intagram</a>
+                <a href="3"><i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+        </div>
+
+        <div class="credit">Credited by <span>mr. web designer</span> | all right reserved! </div>
+
+    </section>
+    <!-- footer section end here  -->
+
+
+    <!-- swiper js link  -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min js"></script>
+
+    <!-- custom js link  -->
+    <script src="js/script.js"></script>
+    
+</body>
+</html>
